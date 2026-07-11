d639632d2eb07bd50da0eb09b61cd8724f4b983d
Yutaka Owada <djkotori@gmail.com>
2026-07-11 21:35:03 +0900

    Added instructions on how to edit voices to the guide. Added setup for Visual Studio Code to the guide.

--- astro.config.mjs
+++ astro.config.mjs
@@ -90,10 +90,11 @@ export default defineConfig({
 					label: '開発者の方向け', translations: { 'en': 'For Developers' },
 					collapsed: true,
 					items: [
-						{ label: 'プラグインでゲームやツールの拡張をしよう', translations: { 'en': 'Example Guide' }, slug: 'devel/1-plugin-intro' },
-						{ label: 'sakana言語について', translations: { 'en': 'Example Guide' }, slug: 'devel/2-sakana-lang-specs' },
-						{ label: 'RPG-Coboのフォルダ構造', translations: { 'en': 'Example Guide' }, slug: 'devel/3-folder-structure' },
-						{ label: 'プラグインの作り方', translations: { 'en': 'Example Guide' }, slug: 'devel/4-plugin-basics' },
+						{ label: 'プラグインでゲームやツールの拡張をしよう', translations: { 'en': 'Expand your games and tools with Plugins' }, slug: 'devel/1-plugin-intro' },
+						{ label: 'sakana言語について', translations: { 'en': 'About the sakana language' }, slug: 'devel/2-sakana-lang-specs' },
+						{ label: 'RPG-Coboのフォルダ構造', translations: { 'en': 'RPG-Cobo Folder Structure' }, slug: 'devel/3-folder-structure' },
+						{ label: 'プラグインの作り方', translations: { 'en': 'How to create a plugin' }, slug: 'devel/4-plugin-basics' },
+						{ label: 'VSCodeの開発環境の構築', translations: { 'en': 'Setup for development in VSCode' }, slug: 'devel/5-vscode' },
 						// { label: 'RPG-良く使うクラスや関数', translations: { 'en': 'Example Guide' }, slug: 'devel/4-common-classes-functions' },
 						{
 							
